1b317e356ef535eca94a8f3fa44f3314a118f2db
fanyuhan1997 <2567615185@qq.com>
2024-09-14 16:59:30 +0800

    origin

---
+++ MGndeSadjointFoam/BoundaryConditions/NeutronFluxCoupledBase/NeutronFluxCoupledBase.C
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "NeutronFluxCoupledBase.H"
+#include "fluidThermo.H"
+#include "solidThermo.H"
+#include "thermophysicalTransportModel.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::NeutronFluxCoupledBase::NeutronFluxCoupledBase
+(
+    const fvPatch& patch
+)
+:
+    patch_(patch),
+    myDFieldName_("undefined-myDFieldName"),
+    nbrDFieldName_("undefined-nbrDFieldName"),
+    myAlpha_(patch.size(), Zero),
+    nbrAlpha_(patch.size(), Zero)
+
+{}
+
+
+Foam::NeutronFluxCoupledBase::NeutronFluxCoupledBase
+(
+    const fvPatch& patch,
+    const dictionary& dict
+)
+:
+    patch_(patch),
+    myDFieldName_(dict.lookup("myDiffFieldName")),
+    nbrDFieldName_(dict.lookup("nbrDiffFieldName")),
+    myAlpha_("myAlpha", dict, patch.size()),
+    nbrAlpha_("nbrAlpha", dict, patch.size())
+{}
+
+
+Foam::NeutronFluxCoupledBase::NeutronFluxCoupledBase
+(
+    const fvPatch& patch,
+    const NeutronFluxCoupledBase& base
+
+)
+:
+    patch_(patch),
+    myDFieldName_(base.myDFieldName_),
+    myAlpha_(base.myAlpha_),
+    nbrAlpha_(base.nbrAlpha_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::scalarField> Foam::NeutronFluxCoupledBase::Diff
+(
+    const fvPatchScalarField& Fluxp
+) const
+{
+
+    const fvMesh& mesh = patch_.boundaryMesh().mesh();
+    const label patchi = patch_.index();
+
+    if (mesh.foundObject<volScalarField>(myDFieldName_))
+    {
+        return ((1+myAlpha_)*(patch_.lookupPatchField<volScalarField, scalar>
+        (
+          myDFieldName_
+        )));
+    }
+    else if (mesh.foundObject<volScalarField>(nbrDFieldName_))
+    {
+        return ((1+nbrAlpha_)*(patch_.lookupPatchField<volScalarField, scalar>
+        (
+          nbrDFieldName_
+        )));
+    }
+    else
+    {
+        FatalErrorInFunction
+              << "Did not find field " << myDFieldName_
+              << "or field " << nbrDFieldName_
+              << " on mesh " << mesh.name() << " patch " << patch_.name()
+              << exit(FatalError);
+    }
+
+}
+
+
+
+
+void Foam::NeutronFluxCoupledBase::write(Ostream& os) const
+{}
+
+
+// ************************************************************************* //
